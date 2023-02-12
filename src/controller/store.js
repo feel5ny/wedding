7ce4f9d0ace3bc6b75faa5b740807f3c9ec8999a
Joy Kim <30822283+feel5ny@users.noreply.github.com>
2023-02-13 01:07:03 +0900

    확장자 수정

--- src/controller/store.js
+++ src/controller/store.js
@@ -1,4 +1,4 @@
 export const IMAGE_PNG_LIST = [];
 export const IMAGE_JPG_LIST = [];
 export const IMAGE_HERO_URL_LIST = [];
-export const IMAGE_HERO_LIST = ["png/09.png", "png/04.png", "png/07.png"];
+export const IMAGE_HERO_LIST = ["png/09.webp", "png/04.webp", "png/07.webp"];
