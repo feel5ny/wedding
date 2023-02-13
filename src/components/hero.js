a1ec1ba1c3652908878e55bebcb53933c8748054
Joy Kim <30822283+feel5ny@users.noreply.github.com>
2023-02-13 11:06:14 +0900

    QA 수정

--- src/components/hero.js
+++ src/components/hero.js
@@ -32,7 +32,7 @@ function addEventListener() {
   });
   $introDirectCalendar.addEventListener("click", function () {
     window.location.href =
-      "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20230430T123000/20230430T133000&text=%EB%82%98%EC%98%81-%EC%88%9C%EC%98%A4+%EA%B2%B0%ED%98%BC%EC%8B%9D+%F0%9F%92%8D&location=%EC%B2%9C%EC%A3%BC%EA%B5%90%EC%84%B1%ED%94%84%EB%9E%80%EC%B9%98%EC%8A%A4%EC%BD%94%ED%9A%8C%EC%88%98%EB%8F%84%EC%9B%90%EA%B5%90%EC%9C%A1%ED%9A%8C%EA%B4%80&details=www.naver.com";
+      "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20230430T123000/20230430T133000&text=%EB%82%98%EC%98%81-%EC%88%9C%EC%98%A4+%EA%B2%B0%ED%98%BC%EC%8B%9D+%F0%9F%92%8D&location=%EC%B2%9C%EC%A3%BC%EA%B5%90%EC%84%B1%ED%94%84%EB%9E%80%EC%B9%98%EC%8A%A4%EC%BD%94%ED%9A%8C%EC%88%98%EB%8F%84%EC%9B%90%EA%B5%90%EC%9C%A1%ED%9A%8C%EA%B4%80&details=https://feel5ny.wedding";
   });
 }
 
